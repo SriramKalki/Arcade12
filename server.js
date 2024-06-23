197aca7e0409988bd6b58e32b8b2e69def565bc6
SriramKalki <sriramkalki@hotmail.com>
2024-06-23 00:08:55 -0500

    Setting Up Node/NPM on my computer for the first time and creating an express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static('public'));
+
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
